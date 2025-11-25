99c69b7e24692b618d74b4bb59906589c3070ed8
Oliver Lavers <oliver.f.lavers@gmail.com>
2025-11-25 10:08:20 +1000

    script

---
+++ startup.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+set -e
+
+RED=$(tput setaf 1)
+NORMAL=$(tput sgr0)
+PROJECT_NAME=${PWD##*/}
+PROJECT_DIR=${PWD}
+
+if ! command -v uv >/dev/null 2>&1; then 
+    printf "${RED}ERROR: UV package manager not found. ${NORMAL}"
+    printf "\nAborting. Install UV with"
+    printf "\n\n\tcurl -LsSf https://astral.sh/uv/install.sh | sh\n\n"
+    printf "before running this script again!\n"
+    exit 1
+fi
+
+uv init .
+uv add dbt-duckdb duckdb
+cat >> .gitignore <<EOL
+*.duckdb
+.env
+EOL
+
+DBT_PROFILES_DIR="${PROJECT_DIR}/.dbt"
+echo "DBT_PROFILES_DIR=${DBT_PROFILES_DIR}" >> .env
+echo 'export UV_ENV_FILE="${PROJECT_DIR}/.env"' >> .venv/bin/activate
+
+mkdir .dbt
+touch .dbt/profiles.yml
+
+uv run --env-file .env dbt init $PROJECT_NAME
+mv $PROJECT_NAME/*
+rm -r $PROJECT_NAME
+
+cat > ./dbt_project.yml <<EOL
+name: ${PROJECT_NAME}
+version: '1.0'
+config-version: 2
+
+profile: ${PROJECT_NAME}
+
+model-paths: ["models"]
+EOL
+
+cat > create_db.py << EOL
+import duckdb
+with duckdb.connect("dev.duckdb") as con:
+    pass
+EOL
+
+
+uv run create_db.py
+
+rm create_db.py
+
+mkdir models
+
+cat > ./models/schema.yml <<EOL
+version: 2
+
+sources:
+- name: dev_db
+    schema: main
+    tables:
+#   - name: <insert_name_here>
+EOL
+
+rm -- "$0"
